c442db13d2604953276ca8afeed69d7a3599bfa6
Amir Yousef <ayousef@tampabay.rr.com>
2018-03-06 13:36:40 -0500

    Added all files

---
+++ Project Requirements.js
@@ -0,0 +1,41 @@
+Requirements
+Create an XML document
+To complete this project, you will need to create an XML document in Android Studio.
+
+Include a ViewGroup
+Be sure to include a ViewGroup (RelativeLayout or LinearLayout) and enough views (Image, Text, or Button views) to complete the project. Check your project against the rubric provided in this lesson.
+
+Important Specifications
+Name your project HelloAndroid
+
+Your design must include the following text (business name, address, and phone number):
+
+Udacity
+2465 Latham St
+Mountain View, CA 94043
+650-555-5555
+
+You may also include Udacity's website: www.udacity.com, or any other descriptive text about Udacity (this is optional)
+
+IMAGE: You can use any image you like that you feel represents Udacity or what Udacity does. Feel free to be creative!
+
+
+***Project Rubric***
+
+In order for your project to pass, it must meet the following specifications:
+
+1. Basic Project Requirements - Project is named HelloAndroid
+
+2. Design Requirements - Design includes, at minimum, the following text (business name, address, and phone number):
+Udacity
+2465 Latham St
+Mountain View, CA 94043
+650-555-5555
+
+3. Design includes at least one image that the student feels represents the business in some way (creativity is fine, and encouraged!)
+
+4. Code Requirements:
+At least one ViewGroup is included
+XML must include a RelativeLayout or LinearLayout
+ViewGroups contain at least one ImageView and at least one TextView
+The project must have one or more ImageViews and TextViews
